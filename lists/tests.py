1c9605cde1640090229aaef164bc82afc410f74b
luiseufrasio <luis.eufrasio@gmail.com>
2015-10-13 00:15:23 -0300

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -6,7 +6,21 @@ from lists.views import home_page
 from lists.models import Item
 
 # Create your tests here.
-class ItemModelTest(TestCase):
+class ListViewTest(TestCase):
+	def test_uses_list_template(self):
+		response = self.client.get('/lists/the-only-list-in-the-world/')
+		self.assertTemplateUsed(response, 'list.html')
+
+	def test_display_all_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		response = self.client.get('/lists/the-only-list-in-the-world/')
+		
+		self.assertContains(response, 'itemey 1')
+		self.assertContains(response, 'itemey 2')
+		
+class ItemModelTest(TestCase):	
 	def test_saving_and_retrieving_items(self):
 		first_item = Item()
 		first_item.text = 'The first (ever) list item'
@@ -24,18 +38,7 @@ class ItemModelTest(TestCase):
 		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
 		self.assertEqual(second_saved_item.text ,'Item the second')
 
-class HomePageTest(TestCase):
-	
-	def test_home_page_displays_all_list_items(self):
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
-		
-		request = HttpRequest();
-		response = home_page(request)
-		
-		self.assertIn('itemey 1', response.content.decode())
-		self.assertIn('itemey 2', response.content.decode())
-		
+class HomePageTest(TestCase):		
 	def test_home_page_only_saves_items_when_necessary(self):
 		request = HttpRequest()
 		home_page(request)
@@ -70,4 +73,4 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 		
 		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/')
+		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
